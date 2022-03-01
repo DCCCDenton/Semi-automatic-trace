0d70eb245b6cc70dc13af9c94f054b2fb61b920d
DCCCDenton <DCCCDenton@gmail.com>
2022-03-01 16:11:55 +0200

    Add project files.

---
+++ Main Class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System.Reflection;
+
+namespace Semi_automatic_trace
+{
+    public class CsAddPnBtn : IExternalApplication
+    {
+        // Both OnStartup and OnShutdown must be implemented as public method
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("Semi_automatic_trace");
+
+            // Create a push button to trigger a command add it to the ribbon panel.
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("cmdSemi Automatic Trace", "Automatic Trace", thisAssemblyPath, "Semi_automatic_trace.Semi__automatic_trace");
+
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            // Optionally, other properties may be assigned to the button
+            // a) tool-tip
+            pushButton.ToolTip = "Semi_automatic_trace";
+
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // nothing to clean up in this simple case
+            return Result.Succeeded;
+        }
+    }
+
+    [TransactionAttribute(TransactionMode.Manual)]  //активация транзакции
+    [RegenerationAttribute(RegenerationOption.Manual)]// активация регенрации 
+    public class Semi__automatic_trace : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData externalCommand, ref string message, ElementSet elements)
+        {
+            Document doc = externalCommand.Application.ActiveUIDocument.Document;
+            UIDocument uidoc = externalCommand.Application.ActiveUIDocument;
+            Main_Wind userWind = new Main_Wind(doc, uidoc);
+            userWind.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
+
---
+++ Main_Wind.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using System.Collections;
+using QuickGraph;
+using GraphSharp;
+
+
+namespace Semi_automatic_trace
+{
+    /// <summary>
+    /// Interaction logic for Main_Wind.xaml
+    /// </summary>
+    public partial class Main_Wind : Window
+    {
+        Document _doc;
+        UIDocument _uidoc;
+        public Main_Wind(Document doc, UIDocument uidoc)
+        {
+            InitializeComponent();
+            _doc = doc;
+            _uidoc = uidoc;
+        }
+
+        public void B_Graph(Object sender, EventArgs e)
+        {
+            MEPElements Mep_Elements = new MEPElements();
+            BidirectionalGraph<object, IEdge<object>> graph = Mep_Elements.BuildGraph(_doc, _uidoc);
+            GraphLayout.Graph = graph;
+        }
+        
+
+
+
+    }
+}
