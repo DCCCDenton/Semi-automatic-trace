1e01a8d1b56a6c9f86ade7bb7f9ef310c535b31c
DCCCDenton <DCCCDenton@gmail.com>
2023-09-05 00:16:41 +0200

    tracing

--- Services/Tracing.cs
+++ Services/Tracing.cs
@@ -12,13 +12,48 @@ namespace Semi_automatic_trace.Services
 {
     internal class Tracing
     {
-        public Element GetClosestElementInRoom(Room room, ElectricalSystemElements electricalSystemElements)
+
+        public List<Element> TraceRoom(Room room, ElectricalSystemElements electricalSystemElements)
         {
-            Element closestElement = null;
+            List<Element> listOfTracingElements = new();
             List<Element> elementInRoom = electricalSystemElements.FeedElectricalElements.Where(e => (e as FamilyInstance).Room.Id == room.Id).ToList();
+            Element firstElement = GetClosestElements(room, elementInRoom, electricalSystemElements.Panel);
+            listOfTracingElements.Add(firstElement);
+            List<Element> removeList = new();
+            removeList.Add(firstElement);   
             foreach (Element element in elementInRoom)
             {
-                element.
+                List<Element> newList = RemoveElement(elementInRoom, removeList);
+                foreach (Element elm in newList)
+                {
+                    Element closestElement = GetClosestElements(room, elementInRoom, firstElement);
+                    firstElement = closestElement;
+                    removeList.Add(firstElement); 
+                }               
+            }
+            return listOfTracingElements;
+        }
+
+        private List<Element> RemoveElement(List<Element> sourceList, List<Element> removeList)
+        {
+            foreach (Element element in removeList)
+            {
+                sourceList.Remove(element);
+            }
+            return sourceList;
+        }
+        private Element GetClosestElements(Room room, List<Element> elementInRoom, Element sourceElement)
+        {
+            Element closestElement = null;            
+            double minDistance = double.MaxValue;
+            foreach (Element element in elementInRoom)
+            {
+                double distance = DistanceBetweenElements(sourceElement, element);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestElement = element;
+                }
             }
             return closestElement;
         }
@@ -28,7 +63,7 @@ namespace Semi_automatic_trace.Services
             double distance = 0;
             XYZ locationElement1 = ((element1.Location) as LocationPoint).Point;
             XYZ locationElement2 = ((element2.Location) as LocationPoint).Point;
-            distance locationElement1.DistanceTo(locationElement2);
+            distance = locationElement1.DistanceTo(locationElement2);
             return distance;
         }
     }
